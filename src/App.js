8b3ae6a858b8d3d83ac69817f489096f114e880f
Ivan Berezovskii <iam.berezovsky@gmail.com>
2024-04-20 18:12:37 +0400

    add mock images

--- src/App.js
+++ src/App.js
@@ -5,9 +5,6 @@ import Leaderboard from './Leaderboard';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Топ-5 Пользователей</h1>
-      </header>
       <Leaderboard />
     </div>
   );
