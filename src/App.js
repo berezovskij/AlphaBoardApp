b0267ca16e1877eb2379d54bcee3bac8b0074f17
Ivan Berezovskii <iam.berezovsky@gmail.com>
2024-04-20 20:48:32 +0400

    clean code and update view

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import './App.css';
 import Leaderboard from './Leaderboard';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Leaderboard />
     </div>
   );
--- src/Leaderboard.js
+++ src/Leaderboard.js
@@ -6,28 +6,38 @@ import CharoitImage from './images/Charoit.png';
 import ZinciteImage from './images/Zincite.png';
 
 const users = [
-  { id: 1, name: 'Иван Иванов', photo: CerussiteImage , rank: 'topUser' },
-  { id: 2, name: 'Петр Петров', photo: CharoitImage, rank: 'secondUser' },
-  { id: 3, name: 'Алексей Алексеев', photo: ZinciteImage, rank: 'thirdUser' },
-  { id: 4, name: 'Ольга Семенова', rank: 'normalUser' },
-  { id: 5, name: 'Мария Лопухова', rank: 'normalUser' }
+  { id: 1, name: 'Иван Иванов', photo: CerussiteImage },
+  { id: 2, name: 'Петр Петров', photo: CharoitImage },
+  { id: 3, name: 'Алексей Алексеев', photo: ZinciteImage },
+  { id: 4, name: 'Ольга Семенова' },
+  { id: 5, name: 'Мария Лопухова' }
 ];
 
 const Leaderboard = () => {
   return (
     <div className={styles.leaderboard}>
-      <h1 className={styles.title}>Топ-5 Пользователей</h1>
-      {users.map((user) => (
-        <div key={user.id} className={`${styles.profile} ${styles[user.rank]}`}>
-          {user.photo && <img src={user.photo} alt={user.name} className={`${styles.photo} ${styles[user.rank + 'Photo']}`} />}
-          <div className={styles.name}>{user.name}</div>
-        </div>
+      <header>
+        <h1 className={styles.title}>Топ-5 Пользователей</h1>
+      </header>
+      
+      <section id={`user-${users[0].id}`} className={styles.topUserContainer}>
+        <img src={users[0].photo} alt={users[0].name} className={styles.topUserPhoto} />
+        <div className={styles.topUserName}>{users[0].name}</div>
+      </section>
+      
+      {users.slice(1, 3).map(user => (
+        <section id={`user-${user.id}`} key={user.id} className={styles.secondThirdUserContainer}>
+          <img src={user.photo} alt={user.name} className={styles.smallPhoto} />
+          <div className={styles.secondThirdUserName}>{user.name}</div>
+        </section>
       ))}
+
+      <section className={styles.fourthFifthUserContainer}>
+        <div id={`user-${users[3].id}`} className={styles.fourthFifthUserName}>{users[3].name}</div>
+        <div id={`user-${users[4].id}`} className={styles.fourthFifthUserName}>{users[4].name}</div>
+      </section>
     </div>
   );
 };
-
-
-
 
 export default Leaderboard;
