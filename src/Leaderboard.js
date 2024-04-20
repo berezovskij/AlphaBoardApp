9cc65b72ca6b2486a5bff6710dfee086b5fe15f5
Ivan Berezovskii <iam.berezovsky@gmail.com>
2024-04-20 17:27:27 +0400

    change board view

--- src/Leaderboard.js
+++ src/Leaderboard.js
@@ -12,12 +12,18 @@ const users = [
 const Leaderboard = () => {
   return (
     <div className={styles.leaderboard}>
-      {users.slice(0, 3).map(user => (
-        <div key={user.id} className={`${styles.profile} ${styles[user.rank]}`}>
-          {user.photo && <img src={user.photo} alt={user.name} className={styles.photo} />}
-          <div className={styles.name}>{user.name}</div>
-        </div>
-      ))}
+      <div className={`${styles.profile} ${styles.topUser}`}>
+        {users[0].photo && <img src={users[0].photo} alt={users[0].name} className={styles.photo} />}
+        <div className={styles.name}>{users[0].name}</div>
+      </div>
+      <div style={{ display: 'flex', width: '100%' }}>
+        {users.slice(1, 3).map(user => (
+          <div key={user.id} className={`${styles.profile} ${styles[user.rank]}`}>
+            {user.photo && <img src={user.photo} alt={user.name} className={styles.photo} />}
+            <div className={styles.name}>{user.name}</div>
+          </div>
+        ))}
+      </div>
       <div className={styles.normalUsersContainer}>
         {users.slice(3).map(user => (
           <div key={user.id} className={`${styles.profile} ${styles[user.rank]}`}>
